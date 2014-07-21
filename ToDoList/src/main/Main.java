18faa88f8a619a5d51823eff70123af107703082
Pia-Git <p.c.sinzig@googlemail.com>
2014-07-21 18:26:27 +0200

    third commit

---
+++ ToDoList/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import elements.ToDoListe;
+
+/**
+ * Die Klasse Main fuehrt die Applikation aus.
+ * 
+ * @author Pia
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		ToDoListe app = new ToDoListe();
+	}
+
+}
+
